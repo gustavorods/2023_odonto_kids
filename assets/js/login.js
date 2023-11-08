49661a0c7bff43e54156ee86f3b0a28b8f816daa
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-11-08 00:25:48 -0300

    <feat> [area de cadastrar]: implementação do botão de próximo e voltar

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,5 @@
+// Rotação do toggle
+
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register')
 const loginBtn = document.getElementById('login');
@@ -8,4 +10,26 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
+})
+
+// Formulario de cadastro (botão de próximo e voltar)
+const titulo_cadastrar = document.querySelector("#titulo-cadastrar");
+const primeiros_inputs = document.querySelector(".input-first-info");
+const segundo_inputs = document.querySelector(".input-second-info");
+const area_btn_back_next = document.querySelector(".btn-form-back-next");
+const btn_proximo = document.querySelector("#btn-form-next");
+const btn_voltar = document.querySelector("#btn-form-back"); 
+
+btn_proximo.addEventListener('click', () => {
+    titulo_cadastrar.innerHTML = "Quase lá..."
+    primeiros_inputs.style.display = "none";
+    segundo_inputs.style.display = "block";
+    btn_voltar.style.display = "inline"
+})
+
+btn_voltar.addEventListener('click', () => {
+    titulo_cadastrar.innerHTML = "Cadastrar-se"
+    primeiros_inputs.style.display = "block";
+    segundo_inputs.style.display = "none";
+    btn_voltar.style.display = "none"
 })
