68286f6e0d6bf2d7ca563f8e2050f74ae8e0f668
Harthuz <hernandeshass910@gmail.com>
2024-03-04 11:20:27 -0300

    feat adicionando botão enviar contato

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -7,3 +7,29 @@ botoes.forEach((botao) => {
         window.location.href = link;
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var campoMensagem = document.querySelector(".input-mensagem");
+    var botaoEnviar = document.getElementById("botaoEnviar");
+    var campoNome = document.querySelector(".input");
+    var campoTelefone = document.querySelector(".input-double");
+    var campoObjetivo = document.querySelector(".input-double");
+    var campoMensagem = document.querySelector(".input-mensagem");
+    var botaoEnviar = document.getElementById("botaoEnviar");
+
+    campoMensagem.addEventListener("input", function() {
+        if (campoMensagem.value.trim() !== "") {
+            botaoEnviar.style.display = "block";
+        } else {
+            botaoEnviar.style.display = "none";
+        }
+    });
+
+    botaoEnviar.addEventListener("click", function() {
+        if (campoNome.value.trim() === "" || campoTelefone.value.trim() === "" || campoObjetivo.value.trim() === "" || campoMensagem.value.trim() === "") {
+            alert("Por favor, preencha todos os campos antes de enviar.");
+        } else {
+          
+        }
+    });
+});
