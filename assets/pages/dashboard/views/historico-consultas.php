4792d90bf74528714c7edcc6c8139279812ac1f9
Hernandes <Hernandeshass910@gmail.com>
2024-11-02 16:43:14 -0300

    style: cria estilo para card e separa os estilos de sexo para duas outras classes

--- assets/pages/dashboard/views/historico-consultas.php
+++ assets/pages/dashboard/views/historico-consultas.php
@@ -13,40 +13,39 @@
         width: 700px;
     }
 
-    .card-boy{
-        margin: 20px 0px;
-        border-radius: 15px;
-        box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.315);
-        padding: 10px 40px;
+    .girl{
+        background-color: #FBD2FF;
+    }
+
+    .boy{
         background-color: #D2EAFF;
     }
 
-    .card-girl{
+    .card{
         margin: 20px 0px;
         border-radius: 15px;
+        border: 0px;
         box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.315);
         padding: 10px 40px;
-        background-color: #FBD2FF;
     }
 
     .corpo-card-horizontal{
         margin-left: 10px;
     }
 
-    .line-boy{
-        background: #0681F3;
+    .line{
         width: 3px;
-        height: 168px;
+        height: 144px;
         position: absolute;
         transform: translate(-8px, -10px);
     }
 
-    .line-girl{
+    .boy .line{
+        background: #0681F3;
+    }
+
+    .girl .line{
         background: #E336DC;
-        width: 3px;
-        height: 168px;
-        position: absolute;
-        transform: translate(-8px, -10px);
     }
 
     .data-status{
@@ -60,11 +59,11 @@
         color: #636363;
     }
 
-    .card-boy .data-status .status{
+    .boy .data-status .status{
         color: #2E81C9;
     }
 
-    .card-girl .data-status .status{
+    .girl .data-status .status{
         color: #FF55F8;
     }
 
@@ -105,18 +104,28 @@
         margin-right: 4px;
     }
 
-    .card-girl .perfil-detalhes .botao-detalhes .detalhes-historico-consulta{
+    .perfil-detalhes .botao-detalhes .detalhes-historico-consulta{
+        background-color: #0681F3;
+        padding: 8px 20px;
+        border-radius: 8px;
+        color: white;
+    }
+
+    .girl .perfil-detalhes .botao-detalhes .detalhes-historico-consulta{
         background-color: #FF55F8;
     }
 
-    .card-boy .perfil-detalhes .perfil-imagem img{
+    .boy .perfil-detalhes .botao-detalhes .detalhes-historico-consulta{
+        background-color: #0681F3;
+    }
+
+    .boy .perfil-detalhes .perfil-imagem img{
         border-color: #0681F3;
     }
 
-    .card-girl .perfil-detalhes .perfil-imagem img{
+    .girl .perfil-detalhes .perfil-imagem img{
         border-color: #E336DC;
     }
-
 
     .nome-perfil{
         font-size: 12pt;
@@ -131,73 +140,20 @@
     <h1>HISTÓRICO DE CONSULTAS:</h1>
 
     <div class="cards-container">
-        <div class="card-boy">
-            <div class="line-boy"></div>
+        
+        <div class="card girl">
+            <div class="line"></div>
 
             <div class="corpo-card-horizontal">
                 <div class="data-status">
 
                     <div class="data">
-                        <p>26 de Setembro</p>
+                        <p>26 de Setembro 14</p>
                     </div>
                     
                     <div class="status">
                         Realizado
                     </div>
-
-                    
-                </div>
-
-                <div class="tipo-endereco">
-                    
-                    <div class="tipo-consulta">
-                        <p>Canal dentário</p>
-                    </div>
-                    
-                    <div class="endereco">
-                        <img src="" alt="">
-                        <p>Av. Pires do rio, 2043</p>
-                    </div>
-                </div>
-
-                <div class="perfil-detalhes">
-                    
-                    <div class="left-container">
-                        <div class="perfil-imagem">
-                            <img src="/2023_odonto_kids/assets/img/home/erick.jpeg" alt="">
-                        </div>
-
-                        <div class="nome-perfil">
-                            <p>Lourenço Alvite</p>
-                        </div>
-                    </div>
-
-                    <div class="botao-detalhes">
-                        <a href="">
-                            <button class="detalhes-historico-consulta">
-                                Detalhes
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-girl">
-            <div class="line-girl"></div>
-
-            <div class="corpo-card-horizontal">
-                <div class="data-status">
-
-                    <div class="data">
-                        <p>26 de Setembro</p>
-                    </div>
-                    
-                    <div class="status">
-                        Realizado
-                    </div>
-
-                    
                 </div>
 
                 <div class="tipo-endereco">
@@ -205,11 +161,7 @@
                     <div class="tipo-consulta">
                         <p>Retirada de cárie</p>
                     </div>
-                    
-                    <div class="endereco">
-                        <img src="" alt="">
-                        <p>Av. Pires do rio, 2043</p>
-                    </div>
+
                 </div>
 
                 <div class="perfil-detalhes">
