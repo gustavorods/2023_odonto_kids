72e8d2f441064a935c38ca59f45b5a11b9246d48
gustavorods <gustavo.rlsilva07@gmail.com>
2024-11-13 18:35:23 -0300

    fix: antes de subir a senha pro banco, transforma ela em hash

--- assets/pages/recuperar_senha_trocar.php
+++ assets/pages/recuperar_senha_trocar.php
@@ -71,6 +71,7 @@
                         $responsavel = new responsavel();
                         $metodos_principais = new metodos_principais();
                         
+                        $senha_hash = password_hash($txt_nova_senha, PASSWORD_DEFAULT);
                         if($txt_nova_senha == $txt_nova_senha_confirmar) {
                             // Verificando qual tabela o email que o usuario usou pertence
                             $informacao_usuario = $metodos_principais->verificar_email_tabela_e_id($_SESSION["txt_email"]);
@@ -78,7 +79,7 @@
                             if($informacao_usuario['tabela'] == "responsavel") {
                                 // Definindo os dados do responsavel
                                 $responsavel->setId($informacao_usuario['id']);
-                                $responsavel->setSenha($txt_nova_senha);
+                                $responsavel->setSenha($senha_hash);
 
                                 // Executando a função para alterar
                                 $responsavel->alterar_senha();
@@ -90,7 +91,7 @@
                             else if($informacao_usuario['tabela'] == "medico") {
                                 // Definindo os dados do responsavel
                                 $medico->setId($informacao_usuario['id']);
-                                $medico->setSenha($txt_nova_senha);
+                                $medico->setSenha($senha_hash);
 
                                 // Executando a função para alterar
                                 $medico->alterar_senha();
