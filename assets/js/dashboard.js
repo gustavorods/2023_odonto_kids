6e8378e02ac66cfda6d238d8a56d7907fdb6b3b0
Hernandes <Hernandeshass910@gmail.comgit>
2024-11-29 10:51:41 -0300

    feat: passa nome paciente para detalhes histórico consulta dashboard responsável

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -101,6 +101,7 @@ const botoesDetalhes = document.querySelectorAll('.detalhes-historico-consulta')
 // Seleciona o modal e os elementos dentro dele
 const modal = document.querySelector('.modal_detalhes_proxima_consulta');
 const pDataConsulta = modal.querySelector('.data-consulta');
+const nomePaciente = modal.querySelector('.paciente-consulta');
 const prontuarioButton = modal.querySelector('#prontuario');
 const relatorioButton = modal.querySelector('#relatorio');
 
@@ -112,15 +113,18 @@ botoesDetalhes.forEach(botao => {
         
         // Dentro do card, encontra o <p> dentro da classe 'data'
         const dataElement = card.querySelector('.data p');
+        const nomeElement = card.querySelector('.nome-perfil p');
         
         // Agora você pode pegar o conteúdo do <p> (que é a data)
         const dataConteudo = dataElement.textContent;
+        const nomeConteudo = nomeElement.textContent;
 
         // Pega o data-id do botão clicado
         const dataId = botao.getAttribute('data-id');
         
         // Preenche o conteúdo do modal
         pDataConsulta.textContent = dataConteudo; // Preenche a data da consulta no modal
+        nomePaciente.textContent = nomeConteudo; // Preenche a data da consulta no modal
         pDataConsulta.setAttribute('data-id', dataId); // Adiciona o data-id ao <p>
 
         // Define o data-id nos botões de ação também
